6f95d729f15631fe08fc80d60c7026bb63bae749
chihebGithub <chiheb.rabeh@hotmail.fr>
2019-11-18 00:16:28 +0100

    commit file

--- ServletApplication/src/com/servlets/tp2/AdminServlet.java
+++ ServletApplication/src/com/servlets/tp2/AdminServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AdminServlet
  */
-@WebServlet("/AdminServlet")
+@WebServlet("/AdminServlet")  
 public class AdminServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- ServletApplication/src/com/servlets/tp2/ClientPizzaServlet.java
+++ ServletApplication/src/com/servlets/tp2/ClientPizzaServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ClientPizzaServlet")
 public class ClientPizzaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+         
     /**
      * @see HttpServlet#HttpServlet()
      */
