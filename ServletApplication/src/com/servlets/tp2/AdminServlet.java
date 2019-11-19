478624de2fe048a2415a3c20254b81bedf426435
chihebGithub <chiheb.rabeh@hotmail.fr>
2019-11-19 17:47:27 +0100

    tp 2 terminé

--- ServletApplication/src/com/servlets/tp2/AdminServlet.java
+++ ServletApplication/src/com/servlets/tp2/AdminServlet.java
@@ -1,6 +1,9 @@
 package com.servlets.tp2;
 
 import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,14 +30,30 @@ public class AdminServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		response.setContentType("text/html");
+		response.setCharacterEncoding( "UTF-8" );
+		response.getWriter().append("<!DOCTYPE html> <html><head><title>AdminPizza </title>")
+		.append("<bodu><p>")
+		.append("Type Pizza :  <strong>").append(((String) request.getAttribute("Specialite")).toUpperCase()).append("</strong>")
+		.append("</p><p>")
+		.append("Prix : ")
+		.append(request.getAttribute("Prix").toString())
+		.append("</body></html>");
+		}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		ServletContext my_context = this.getServletContext();
+		my_context.setAttribute("Specialite",request.getParameter("pizza"));
+		my_context.setAttribute("prixpizza",request.getParameter("txt_prix"));
+		my_context.setAttribute("dateJour", new Date());
+		request.setAttribute("Specialite",request.getParameter("pizza"));
+		request.setAttribute("Prix",request.getParameter("txt_prix"));
+		
+		
 		doGet(request, response);
 	}
 
--- ServletApplication/src/com/servlets/tp2/ClientPizzaServlet.java
+++ ServletApplication/src/com/servlets/tp2/ClientPizzaServlet.java
@@ -1,6 +1,11 @@
 package com.servlets.tp2;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Hashtable;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,14 +32,37 @@ public class ClientPizzaServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ServletContext my_context = this.getServletContext();
+		Date day = (Date)my_context.getAttribute("dateJour");
+		Double prixPizza =Double.parseDouble(my_context.getAttribute("prixpizza").toString());
+		DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+		String today = df.format(day);
+		String b=request.getAttribute("myprix").toString();
+		System.out.println(b);
+		Double prixfinal =prixPizza+Double.parseDouble(b);
+		response.setContentType("text/html");
+		response.setCharacterEncoding( "UTF-8" );
+		response.getWriter().append("<!DOCTYPE html> <html><head><title>ClientPizza </title>")
+		.append("<body><p>").append("Date : ").append(today).append("</p><p>")
+		.append("vous avez choisis :  <strong>").append(((String)my_context.getAttribute("Specialite")).toUpperCase()).append("</strong>")
+		.append("</p><p>").append("votre dessert :").append(request.getAttribute("dessert").toString()).append("</p><p>")
+		.append("Prix total : ")
+		.append(prixfinal.toString())
+		.append("</body></html>");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.setAttribute("dessert",request.getParameter("dessert"));
+		request.setAttribute("Quantite",request.getParameter("txt_qt"));
+		Hashtable<String,Double> prix= new Hashtable <String,Double>();
+		prix.put("italien",1000.000);
+		prix.put("fraise",1000.500);
+		prix.put("banane",2000.300);
+		request.setAttribute("myprix",(prix.get(request.getParameter("dessert").toString()) 
+				* (Integer.parseInt(request.getParameter("txt_qt")))));
 		doGet(request, response);
 	}
 
