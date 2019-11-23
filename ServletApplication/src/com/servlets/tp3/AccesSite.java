c9d0c9dda9ce98034a1d2b73b3c1132a12f226d1
chihebGithub <chiheb.rabeh@hotmail.fr>
2019-11-23 22:20:11 +0100

    test commit

--- ServletApplication/src/com/servlets/tp3/AccesSite.java
+++ ServletApplication/src/com/servlets/tp3/AccesSite.java
@@ -41,7 +41,7 @@ public class AccesSite extends HttpServlet {
 				.append("</strong>")
 				.append("</p>")
 				.append("<div class=\"scrollmenu\">" + 
-						"  <a href=\"#home\">Home</a>" + 
+						"  <a href=\"#home\">Home</a>" +    
 						"  <a href=\"#news\">News</a>" + 
 						"  <a href=\"#contact\">Contact</a>" + 
 						"  <a href=\"#about\">About</a>" + 
