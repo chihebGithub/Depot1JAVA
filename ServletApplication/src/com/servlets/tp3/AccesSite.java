2ec0f9c2cc2a1963a241e90ea5417dc7d2b2adbf
chihebGithub <chiheb.rabeh@hotmail.fr>
2019-11-23 22:13:17 +0100

    message

---
+++ ServletApplication/src/com/servlets/tp3/AccesSite.java
@@ -0,0 +1,66 @@
+package com.servlets.tp3;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class AccesSite
+ */
+@WebServlet("/AccesSite")
+public class AccesSite extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccesSite() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String login =(String) session.getAttribute("log");
+		// TODO Auto-generated method stub
+				response.setContentType("text/html");
+				response.setCharacterEncoding( "UTF-8" );
+				response.getWriter().append("<!DOCTYPE html> <html><head><title>Deconnection </title>")
+				.append("<bodu><p>")
+				.append("<form action=\"Deconnection\" method=\"get\">")
+				
+				.append("Bonjour :  <strong>").append(login.toUpperCase())
+				.append("</strong>")
+				.append("</p>")
+				.append("<div class=\"scrollmenu\">" + 
+						"  <a href=\"#home\">Home</a>" + 
+						"  <a href=\"#news\">News</a>" + 
+						"  <a href=\"#contact\">Contact</a>" + 
+						"  <a href=\"#about\">About</a>" + 
+						"  <a href=\"#support\">Support</a>" + 
+						"  <a href=\"#blog\">Blog</a>" + 
+						"  <a href=\"#tools\">Tools</a>" + 
+						"</div").append("<br> <div>")
+				.append("<input type=\"submit\" value=\"déconnection\" />")
+				.append("</div>")
+				.append("</form>")
+				.append("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ ServletApplication/src/com/servlets/tp3/SimpleCounterServlet.java
@@ -0,0 +1,66 @@
+package com.servlets.tp3;
+
+
+import java.io.IOException;
+
+import java.util.Hashtable;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class SimpleCounterServlet
+ */
+@WebServlet("/SimpleCounterServlet")
+public class SimpleCounterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	Hashtable<String, String> userAuthorized = new Hashtable<String,String>();
+	public void init() throws ServletException {
+		
+		
+		userAuthorized.put("admin", "admin");
+		userAuthorized.put("commercial", "commercial");
+		userAuthorized.put("invite", "invite");
+		
+		
+		}
+	
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+	ServletException, IOException {
+		HttpSession session = request.getSession();
+		String login = request.getParameter("iduser");
+		String pass = request.getParameter("password");
+		if ((userAuthorized.get(login) != null && userAuthorized.get(login).equals(pass)))
+		
+		{
+		
+		 session.setAttribute("log", login);
+			 RequestDispatcher dispatcher = request.getRequestDispatcher("AccesSite");
+			 dispatcher.forward(request, response); }
+			
+		else{
+			 RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
+			 dispatcher.forward(request, response);
+		   }
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
